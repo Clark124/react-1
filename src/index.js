3ab3cb480d2c6ff1c87141dab05db4bedc19ad71
Clark124 <53799906@qq.com>
2017-04-10 14:49:07 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <div>
-    <h1>hello world!</h1>
-    <p>I am Clark</p>
-  </div>
-  ,
-  document.getElementById('root')
-);
 
+// function tick(){
+//   const  element =(
+//     <div>
+//       <h1>hellow, wordl!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById("root")
+//   );
+// }
+
+// setInterval(tick,1000);
+
+// class Welcome extends React.Component{
+//   render(){
+//     return <h1>Hellow,Component</h1>;
+//   }
+// }
+// ReactDOM.render(
+//   <Welcome/>,
+//   document.getElementById('root')
+// )
+
+import Welcome from './Welcome'
+
+ReactDOM.render(
+  <Welcome name="Clark"/>,
+  document.getElementById('root')
+)
+  
