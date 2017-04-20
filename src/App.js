5e26d7343e3ec4b6f3453a537280fa2ece903a7e
Clark124 <53799906@qq.com>
2017-04-20 17:00:44 +0800

    leancloud

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import 'normalize.css';
 import './reset.css';
-import * as localStore from './localStore'
+import UserDialog from './UserDialog'
+import {getCurrentUser,signOut} from './leancloud'
+import AV from './leancloud'
+
+
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
+      user:{},
       newTodo: '',
-      todoList: localStore.load('todoList') || []
+      todoList: [],
+      currentUser:null 
     }
   }
-
+  
+  
   render() {
+    
     let todos = this.state.todoList
       .filter((item) => !item.deleted)
       .map((item,index)=>{
@@ -25,10 +33,11 @@ class App extends Component {
               </li>
             )
     })
-
-    return (
-      <div className="App">
-        <h1>我的待办</h1>
+    let todo = (
+      <div className="todo">
+        <h1>{this.state.user.username||"我"}的待办
+          {this.state.user.id?<button onClick={this.signOut.bind(this)}>登出</button>:null}
+        </h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)} onSubmit={this.addTodo.bind(this)}/>
         </div>
@@ -36,26 +45,110 @@ class App extends Component {
           {todos}
         </ul>
       </div>
+      
+    )
+    return (
+      <div className="App">
+        {this.state.user.id?todo:null}
+        {this.state.user.id?
+          null:
+          <UserDialog 
+          onSignUp={this.onSignUpOrOnSignIn.bind(this)}
+          onSignIn={this.onSignUpOrOnSignIn.bind(this)}/>}    
+      </div>
     )
   }
 
+  fetchTodos(){
+    if(this.state.currentUser){
+    var query = new AV.Query('AllTodos');
+    query.find()
+      .then((todos)=>{
+        let avAlltodos = todos[0]//因为理论上AllTodos只有一个，所有我们取结果的第一项
+        let id = avAlltodos.id
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.todoList = JSON.parse(avAlltodos.attributes.content)//为什么有个attributes？因为从控制台看到的
+        stateCopy.todoList.id = id //为什么给todoList这个数组设置id？因为数组也是对象
+        this.setState(stateCopy)
+      },function(error){
+        console.error(error)
+      })
+    }
+  }
+
+  updataTodos(){
+      let dataString = JSON.stringify(this.state.todoList)
+      let avTodos = AV.Object.createWithoutData('AllTodos',this.state.todoList.id)
+      avTodos.set('content',dataString)
+      avTodos.save().then(()=>{
+        console.log('更新成功')
+      })
+    }
+
+  saveTodos(){
+    let dataString = JSON.stringify(this.state.todoList)
+    var  AVTodos = AV.Object.extend('AllTodos');
+    var avTodos = new AVTodos();
+    var acl = new AV.ACL();
+    acl.setReadAccess(AV.User.current(),true)//只有这个user能读
+    acl.setWriteAccess(AV.User.current(),true)//只有这个user能写
+    avTodos.set('content',dataString);
+    avTodos.setACL(acl)//设置访问控制
+    avTodos.save().then((todo)=>{
+      let stateCopy = JSON.parse(JSON.stringify(this.state))
+      stateCopy.todoList.id = todo.id //一定记得要把id挂到this.todoList上，否测下次就会调用updateTodos了
+      this.setState(stateCopy)
+      console.log('保存成功');
+    },function(error){
+      alert('保存失败')
+    })
+  }
+
+  saveOrUpdateTodos(){
+    if(this.state.todoList.id){
+      this.updataTodos()
+    }else{
+      this.saveTodos()
+    }
+  }
+
+  signOut(){
+    signOut()
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user={}
+    this.setState(stateCopy)
+  }
+ 
+  onSignUpOrOnSignIn(user){
+    let stateCopy = JSON.parse(JSON.stringify(this.state)) 
+    stateCopy.user = user
+    stateCopy.currentUser = getCurrentUser();
+    this.setState(stateCopy)
+    this.fetchTodos()
+    
+  }
   componentDidUpdate(){
-    localStore.save('todoList',this.state.todoList)
+    // this.state.currentUser = getCurrentUser()
+    // this.setState(this.state)
+    // this.fetchTodos()
   }
   delete(event,todo){
     todo.deleted = true
     this.setState(this.state)
+    this.saveOrUpdateTodos()
   }
 
   toggle(e,todo){
     todo.status = todo.status === 'completed'?'':'completed'
     this.setState(this.state)
+    this.saveOrUpdateTodos()
   }
   changeTitle(e){
     this.setState({
       newTodo:e.target.value,
       todoList: this.state.todoList
     })
+
   }
 
   addTodo(event){
@@ -69,6 +162,7 @@ class App extends Component {
       newTodo:"",
       todoList: this.state.todoList
     })
+    this.saveOrUpdateTodos()
   }
 }
 
---
+++ src/leancloud.js
@@ -0,0 +1,59 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'cIpWugkylWCpzh5yJmQp6tLS-gzGzoHsz';
+var APP_KEY = 'X9tGE2h7t48YQclEv2J5a7RT';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV;
+
+export function signUp(username,password,successFn,errorFn){
+  //新建AVUser 对象实例
+  var user = new AV.User();
+  //设置用户名
+  user.setUsername(username)
+  //设置密码
+  user.setPassword(password)
+  //设置邮箱
+  user.signUp().then(function(loginedUser){
+    let user = getUserFromAVUser(loginedUser)
+    successFn(user)
+  },function(error){
+    errorFn.call(null,error)
+  })
+}  
+export function signIn(username,password,successFn,errorFn){
+  AV.User.logIn(username,password).then(function(loginedUser){
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null,user)
+  },function(error){
+    errorFn.call(null,error)
+  })
+
+  return undefined
+}
+
+export function signOut(){
+  AV.User.logOut()
+  return undefined
+}
+
+export function getCurrentUser(){
+  let current = AV.User.current();
+  
+  if(current){
+    let {id,createdAt,attributes:{username}} = current
+    return {id,username,createdAt}
+  }else{
+    return null
+  }
+}
+
+function getUserFromAVUser(AVUser){
+  return {
+    id: AVUser.id,
+    ...AVUser.attributes
+  }
+}
