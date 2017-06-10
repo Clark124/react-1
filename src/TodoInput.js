878b8a21b27000a9b033d2052bd2125ca602e29f
Clark124 <53799906@qq.com>
2017-06-10 18:49:57 +0800

    不含state的组件可以写成函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TodoInput.css'
-export default class TodoInput extends Component {
+/*export default class TodoInput extends Component {
     render(){
         return <input type="text" value={this.props.content} 
         className="TodoInput"
@@ -15,4 +15,22 @@ export default class TodoInput extends Component {
     changeTitle(e){
         this.props.onChange(e)
     }
+}*/
+
+function submit (props,e){
+    if(e.key==='Enter'){
+        props.onSubmit(e)
+    }
+}
+
+function changeTitle (props,e){
+    props.onchange(e)
+}
+
+export default function(props){
+    return <input type="text" value={props.content}
+    className="TodoInput"
+    onChange={changeTitle.bind(null,props)}
+    onKeyPress={submit.bind(null,props)}/>
+    
 }
